07ce48b507c6dcc09d86cea3ada2b68ca6fd4e01
SushrutKhajuria <sushrutkhajuria1995@gmail.com>
2025-05-04 20:26:01 +0530

    LETS OPTIMIZE AND DO IT THE RIGHT WAY

--- Redux Assignment/src/components/Counter.js
+++ Redux Assignment/src/components/Counter.js
@@ -4,6 +4,7 @@ const Counter = () => {
 
    const dispatch = useDispatch(); 
   const counter = useSelector(state => state.counter)
+ const show = useSelector(state=> state.showCounter)
 
   const incrementHandler=()=>{
     dispatch({ type: 'increment'})
@@ -17,18 +18,21 @@ const Counter = () => {
     dispatch({ type: 'increase', amount :2})
   }
 
-  const toggleCounterHandler = () => {};
+  const toggleHandler = () => {
+    dispatch({type:'toggle'})
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {show && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incrementHandler}>Increment By 5</button>
         <button onClick={decrementHandler}>Decrement By 5</button>
         <button onClick={increaseHandler}>Increase By 2</button>
+        
       </div>
-      <button onClick={toggleCounterHandler}>Toggle Counter</button>
+      <button onClick={toggleHandler}>Toggle Counter</button>
     </main>
   );
 };
